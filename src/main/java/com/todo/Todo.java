1fd44318d53eaa43ce836cbd1bf135c6a24a94e9
이승현 <iseunghyeon@iseunghyeons-MacBook-Pro.local>
2023-03-02 09:59:09 +0900

    3/2 applcation.yml h2, jpa 설정추가, Todo Entity 작성

--- src/main/java/com/todo/Todo.java
+++ src/main/java/com/todo/Todo.java
@@ -1,12 +1,24 @@
 package com.todo;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter @Setter
+@Entity
 public class Todo {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
+    @Column
     private String todoName;
 
+    @Column
     private String todoContent;
 
+    @Column
     private boolean todoSuccess;
 }
